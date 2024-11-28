fa9b1d3613c9b32ec17c0d22c434676def44cfc3
Carson Davis <cmd52465@gmail.com>
2024-11-28 03:18:59 -0800

    remove unnecessary code

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,8 @@
-import localFont from "next/font/local";
 import "./globals.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Carson Davis | Software Engineer Portfolio",
@@ -23,11 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
