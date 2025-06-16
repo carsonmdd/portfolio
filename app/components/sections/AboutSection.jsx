3b966bfb4ae5ec8a0999532037cd465e5d8cd9ca
Carson Davis <cmd52465@gmail.com>
2025-06-16 01:10:48 -0700

    add midi-air project

--- app/components/sections/AboutSection.jsx
+++ app/components/sections/AboutSection.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import SkillCard from '../SkillCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { useInView } from 'react-intersection-observer';
-import { skills } from '@/app/data/skillsData';
+import SkillCard from "../SkillCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { useInView } from "react-intersection-observer";
+import { skills } from "@/app/data/skillsData";
 
 const AboutSection = () => {
 	const { ref, inView } = useInView({
@@ -13,22 +13,22 @@ const AboutSection = () => {
 	});
 
 	const delays = {
-		50: 'animation-delay-[50ms]',
-		100: 'animation-delay-[100ms]',
-		150: 'animation-delay-[150ms]',
-		200: 'animation-delay-[200ms]',
-		250: 'animation-delay-[250ms]',
-		300: 'animation-delay-[300ms]',
-		350: 'animation-delay-[350ms]',
-		400: 'animation-delay-[400ms]',
-		450: 'animation-delay-[450ms]',
-		500: 'animation-delay-[500ms]',
-		550: 'animation-delay-[550ms]',
-		600: 'animation-delay-[600ms]',
-		650: 'animation-delay-[650ms]',
-		700: 'animation-delay-[700ms]',
-		750: 'animation-delay-[750ms]',
-		800: 'animation-delay-[800ms]',
+		50: "animation-delay-[50ms]",
+		100: "animation-delay-[100ms]",
+		150: "animation-delay-[150ms]",
+		200: "animation-delay-[200ms]",
+		250: "animation-delay-[250ms]",
+		300: "animation-delay-[300ms]",
+		350: "animation-delay-[350ms]",
+		400: "animation-delay-[400ms]",
+		450: "animation-delay-[450ms]",
+		500: "animation-delay-[500ms]",
+		550: "animation-delay-[550ms]",
+		600: "animation-delay-[600ms]",
+		650: "animation-delay-[650ms]",
+		700: "animation-delay-[700ms]",
+		750: "animation-delay-[750ms]",
+		800: "animation-delay-[800ms]",
 	};
 
 	return (
@@ -36,10 +36,10 @@ const AboutSection = () => {
 			<h1
 				ref={ref}
 				className={`text-center text-5xl font-semibold mb-20 opacity-0 ${
-					inView && 'animate-slide-top'
+					inView && "animate-slide-top"
 				}`}
 			>
-				{'About'}
+				{"About"}
 			</h1>
 			<div
 				ref={ref}
@@ -47,7 +47,7 @@ const AboutSection = () => {
 			>
 				<div
 					className={`flex flex-col justify-center p-6 max-w-[40rem] md:flex-1 opacity-0 ${
-						inView && 'animate-slide-left'
+						inView && "animate-slide-left"
 					}`}
 				>
 					<FontAwesomeIcon
@@ -59,23 +59,23 @@ const AboutSection = () => {
 							Carson Davis
 						</h2>
 						<p className='leading-8 text-2xl'>
-							{"I'm a third year student studying "}
+							{"I'm a fourth year student studying "}
 							<span className='text-emerald-500 font-normal'>
-								{'Software Engineering '}
+								{"Software Engineering "}
 							</span>
-							{'at UC Irvine with a deep passion for '}
+							{"at UC Irvine with a deep passion for "}
 							<span className='text-emerald-500 font-normal'>
-								{'full-stack web development '}
+								{"full-stack web development "}
 							</span>
-							{'and '}
+							{"and "}
 							<span className='text-emerald-500 font-normal'>
-								{'artificial intelligence'}
+								{"artificial intelligence"}
 							</span>
 							{
-								'. With a focus on problem-solving and user-centered design, I strive to create solutions for '
+								". With a focus on problem-solving and user-centered design, I strive to create solutions for "
 							}
 							<span className='text-emerald-500 font-normal'>
-								{'real-world problems'}
+								{"real-world problems"}
 							</span>
 							{
 								". Outside of coding, I like to spend my time playing piano, fishing, and exploring the outdoors. I'm always eager to learn and connect with like-minded individuals and grow as a developer."
